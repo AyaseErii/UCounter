39445506ba74245bcbc4513a1ccbf422a20bb24a
Cocogoat <70917894+AyaseErii@users.noreply.github.com>
2022-12-04 17:15:55 -0500

    change

---
+++ README.md
@@ -0,0 +1,3 @@
+# GF180 Demo
+
+Uses VGA clock demo from https://github.com/mattvenn/vga-clock
---
+++ clean.sh
@@ -0,0 +1,15 @@
+DESIGN_NAME="cntr_example"
+rm -rf ./def/$DESIGN_NAME.def
+rm -rf ./gds/$DESIGN_NAME.gds
+rm -rf ./lef/$DESIGN_NAME.lef
+rm -rf ./lib/$DESIGN_NAME.lib
+rm -rf ./mag/$DESIGN_NAME.mag
+rm -rf ./maglef/$DESIGN_NAME.mag
+rm -rf ./sdc/$DESIGN_NAME.sdc
+rm -rf ./sdf/$DESIGN_NAME.sdf
+#rm -r ./sdf/multicorner
+rm -rf ./spef/$DESIGN_NAME.spef
+#rm -r ./spef/multicorner
+rm -r ./signoff/$DESIGN_NAME
+rm -rf ./spi/lvs/$DESIGN_NAME.spice
+rm -rf ./verilog/gl/{$DESIGN_NAME.v, $DESIGN_NAME.nl.v} 
